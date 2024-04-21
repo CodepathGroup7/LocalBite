faabaaff06c466906415b550b2c641ea35032b69
zhuazhuzz <zhuazhu.zz@gmail.com>
2024-04-21 01:49:32 -0400

    Added nav, homepage tab, event list tab, account page tab

---
+++ app/src/main/java/com/example/localbite/ui/nav_fragments/AccountFragment.kt
@@ -0,0 +1,37 @@
+package com.example.localbite.ui.nav_fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.localbite.R
+
+class AccountFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_accountpage, container, false)
+        return view
+    }
+
+
+    companion object {
+        fun newInstance(): AccountFragment {
+            return AccountFragment()
+        }
+    }
+
+
+}
